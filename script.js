00c4581ca963fc9379b7c28b1511efc8db54f513
Vinicius Ferreira <vinicius@xablau.com.br>
2022-01-14 16:55:14 -0300

    Capturar elementos html manipulaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status');
