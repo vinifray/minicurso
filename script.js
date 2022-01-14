86c348c4ec1cfca824e72c676c6ff7d6e5d40cfe
Vinicius Ferreira <vinicius@xablau.com.br>
2022-01-14 17:44:36 -0300

    Criar consumo de API Rick And Morty

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
